6fa7a331530e2facfc61cc41e57a66c9d1d07a79
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-21 20:54:14 +0530

    003

---
+++ S01_003_Arrays_Basics.cpp
@@ -0,0 +1,18 @@
+/*
+
+An array is a collection of elements of the same type 
+placed in contiguous memory locations that can be individually 
+referenced by using an index to a unique identifier.
+
+*/
+
+/*
+
+int A[5]  - declaring an array A of size 5
+int B[3] = {1, 2, 3} - declaring and initializing an array B of size 3
+
+memory is allocated in the stack 
+
+A[1], A[2] etc can be used to access the value in that index of the array
+
+*/
