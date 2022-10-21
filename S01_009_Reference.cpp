04757b6e3c210d2213cb0922136e7b8f5b6b5f60
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-21 22:53:15 +0530

    009

---
+++ S01_009_Reference.cpp
@@ -0,0 +1,19 @@
+/*
+
+• A reference is just another name for the same variable
+
+Example :
+Int main( )
+{
+    Int a = 10;
+    Int &r = a; - syntax of reference
+    Cout << a ;
+    r++;
+    Cout<< r ;
+    Cout << a ;
+}
+
+• This is used for parameter passing
+• For writing small functions we use reference
+
+*/
