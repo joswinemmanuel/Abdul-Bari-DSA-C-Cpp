3651c70107c04af79043d5b041d216cbc723af6d
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-22 14:25:37 +0530

    013

---
+++ S01_013_Functions.cpp
@@ -0,0 +1,31 @@
+/*
+
+• Function is a piece of code which performs a specific task
+• Grouping instructions is called function
+• They are called as modules or procedures
+• The main task can be divided into several small task in the form of
+functions this type of programming is also called as modular or
+procedural programming
+• It is easy for development
+• A group of programmers can work on a single project using
+functioning
+• Functions provide reusability of code
+• It can be used in other software projects as well
+• you can group function into library
+
+Example :
+int add( int a , int b ) {
+    int c;
+    c = a + b;
+    return c;
+}
+
+Int main ( ) {
+    int x,y,z;
+    x =10;
+    y = 5;
+    z = add x, y;
+    printf(“sum is %d” , z);
+}
+
+*/
