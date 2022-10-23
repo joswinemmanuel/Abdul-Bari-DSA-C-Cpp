9cde4406f2576f8df877503ee9f6c1e986220032
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 13:14:01 +0530

    029

---
+++ S01_029_Template_Classes.cpp
@@ -0,0 +1,8 @@
+/*
+
+Template classes are generic classes in c++
+Template functions are generic functions in c++ 
+that is we can use the same class or function
+for multiple data types, it works for any type of data
+
+*/
