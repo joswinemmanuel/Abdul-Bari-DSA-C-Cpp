ccee6c0c689830470148982c59aa773ed56febc3
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 22:47:08 +0530

    044

---
+++ S04_044_Time_Space_Complexity.c
@@ -0,0 +1,19 @@
+/*
+
+Time complexity is defined as the amount of time taken by 
+an algorithm to run, as a function of the length of the input.
+
+Space complexity refers to the total amount of memory space used by 
+an algorithm/program, including the space of input values for execution.
+
+for(int i=0; i<n; i++) {}   O(n)
+
+for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {}  O(n**2)
+}
+
+similarly 3 for loop      O(n**3)
+
+for(int i=n; i>1; i=i/2) {}   O(log(n))
+
+*/
