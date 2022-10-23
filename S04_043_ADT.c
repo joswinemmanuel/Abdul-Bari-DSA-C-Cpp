40cd60192d5bd3c0cc381c8ecf9eef9f9a140c70
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 21:43:00 +0530

    043

---
+++ S04_043_ADT.c
@@ -0,0 +1,19 @@
+/*
+
+ADT - Abstract Data Type
+
+Data Type
+ - Representation of data
+ - Operations on data
+
+Abstract means hiding internal details
+
+For Ex - let us take List
+
+List can be represented : using
+1) Array 2) Linked List
+
+List operations :
+add(x) , remove(), search(x) ....etc
+
+*/
