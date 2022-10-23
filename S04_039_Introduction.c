e108757abaa484345addfc6daa401b67ea368ca4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 17:24:15 +0530

    039

---
+++ S04_039_Introduction.c
@@ -0,0 +1,32 @@
+/*
+
+Data is an integral part of our applications or programs
+
+ - Data Structures
+ - Data Bases
+ - Data Warehouse
+ - Big Data
+
+Data Structures
+    Is an arrangement of collection of data items
+    so that we can utilize them efficiently, and operations on
+    them can be done efficiently
+
+Data bases
+    When the data is larger in size, or commericial data that is
+    used in businesses like banks, retail stores, schools etc
+    they will have a lot of data, they will have some organised
+    data in the form of database tables or relational data and all
+    that data is stored on the disk.
+
+Data Warehouse
+    An array of disk having large size of data or historical data
+    for any commercial firm is called data warehouse
+        Algorithms written for analysing this data are called as
+    data mining algorithms
+
+Big Data
+    The huge amount of data that is produced through the internet is
+    called as big data.
+
+*/
