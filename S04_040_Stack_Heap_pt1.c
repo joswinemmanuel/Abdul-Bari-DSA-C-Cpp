30b22c1ebf4490bed7cfb74e4b0217d1df78c82f
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 17:59:04 +0530

    040

---
+++ S04_040_Stack_Heap_pt1.c
@@ -0,0 +1,21 @@
+/*
+         ___________________
+        |       stack       |
+        |-------------------|                
+        |       Heap        |   
+        |-------------------|
+        |    Code Section   |
+        |___________________|
+A program uses the main memory by dividing the code into 
+3 sections 
+ - Code Section
+ - Stack
+ - Heap
+
+Stack Frame of main function or Activation Record of main function
+whatever variables that are created in the function it is created in the stack
+
+    Size of the memory needed was decided before compile time is called
+    Static memory allocation as it cannot be changed
+
+*/
