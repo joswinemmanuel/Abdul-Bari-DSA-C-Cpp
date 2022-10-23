d9343c516e45a7f1f69e750c5fa6b5fc4e3ce2ed
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-23 21:10:13 +0530

    042

---
+++ S04_042_Physical_Logical_DS.c
@@ -0,0 +1,28 @@
+/*
+
+Types of Data Structures
+1) Physical Data Structures
+2) Logical Data Structures
+
+Physical data structures
+ these store the data
+ these data structres defines or decides how the memory is organised or allocated
+ - Arrays 
+    (fixed size) (static size)
+    can be created in stack or in heap
+ - Linked List 
+    is a dynamic data structure
+    size can be increased and decreased
+    can be created in heap
+
+Logical Data Structures
+  Logical ds are implemented using physical ds
+  The discipline or different operations that can be performed
+  are defined by these logical ds
+    1) Stack  - linear ds
+    2) Queue  - linear ds
+    3) Tree   - non linear ds
+    4) Graph  - non linear ds
+    5) Hash table  - tabular ds
+
+*/
