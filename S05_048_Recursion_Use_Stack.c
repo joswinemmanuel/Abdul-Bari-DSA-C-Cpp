1274b1a3e0b4b096c724410f6f95d8a61084dd30
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-24 11:42:16 +0530

    048

---
+++ S05_048_Recursion_Use_Stack.c
@@ -0,0 +1,9 @@
+/*
+
+For each calls in a recursion...each will have it's
+own activation record in the stack and will be deleted
+once each call is finished
+
+Recursive functions are memory consuming functions
+
+*/
