3623a2bb3cba186978c55661e2cd58d92f2fac95
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-24 11:31:29 +0530

    047

---
+++ S05_047_Generalising_Recursion.c
@@ -0,0 +1,11 @@
+/*
+
+void fun(int n) {
+    if(n > 0) {
+        1) execute during Calling time  --- Ascending
+        fun(n-1)
+        2) execute at Returning time    --- Descending
+    }
+}
+
+*/
