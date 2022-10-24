a4c651284f8bdb1b1534db78305ab9b15d6ead58
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-24 13:05:06 +0530

    050

---
+++ S05_050_Recursion_Code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void fun1(int n) {
+    if(n>0) {
+        printf("%d ", n);
+        fun1(n-1);
+    }
+}
+
+void fun2(int n) {
+    if(n>0) {
+        fun2(n-1);
+        printf("%d ", n);
+    }
+}
+
+int main() {
+    int x = 3;
+    fun1(x);
+    printf("\n");
+    fun2(x);
+}
+
+/* 
+3 2 1 
+1 2 3
+*/
