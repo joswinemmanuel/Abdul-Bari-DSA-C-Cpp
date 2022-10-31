eaa77acb08ed380ba6e7f39023b2298c700c4d72
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-31 18:49:24 +0530

    089

--- S06_089_Row_Major_Formula_2D_Array.cpp
+++ S06_089_Row_Major_Formula_2D_Array.cpp
@@ -9,7 +9,7 @@
   In this, the elements are stored row by row 
 address(A[1][2]) = 200 + (4*1 + 2) * 2 = 212
 address(A[2][3]) = 200 + (4*2 + 3) * 2 = 222
-address(A[1][2]) = Lo + (n*i + j) * w
+address(A[i][j]) = Lo + (n*i + j) * w
 where Lo is base address, w is size of data type, n is no. of columns,
 i is first index, j is second index
 
