c3667939f50cec284b9499c2be77cf3172f41fa8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-10 22:34:00 +0530

    106

---
+++ S07_106_Analysis_Binary_Search.cpp
@@ -0,0 +1,10 @@
+/*
+
+• For successful search
+    The best case time for this problem is min - O(1)
+    The worst case time for this problem is max - O(logn)
+
+• For unsuccessful search
+    Its always O(log n) weather the call be recursive or iterative
+    
+*/
