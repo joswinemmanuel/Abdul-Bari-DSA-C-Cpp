4873cd3f28d9870d9bc18758e7043f132dbe0979
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-04-11 22:06:30 +0530

    107

--- S07_107_Average_Case_Analysis_Binary_Search.cpp
+++ S07_107_Average_Case_Analysis_Binary_Search.cpp
@@ -4,5 +4,18 @@ Average Case Analysis of Binary Search
 
  - Average unsuccessful search is O(log n)
  - Average successful search is O(log n)
+ 
+• For finding the average case time we have to first consider the
+internal nodes , the no of comparison for internal nodes depends on
+the level of tracing tree the circular nodes ( I ) are the internal nodes
+which are successful
+• The square nodes are the unsuccessful ones ( E )
+E = nlogn
+E = I + 2n
+e = I + 1
+As (n) = 1 + I / n
+Au (n) = E / n + 1 = nlogn / n + 1
+• The average unsuccessful time is logn
+• The average successful time is also logn
 
 */
