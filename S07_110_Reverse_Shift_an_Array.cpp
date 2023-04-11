5c114142c84c9ef7f890d3170e26c6ead47ecbc8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-04-11 22:51:08 +0530

    110

---
+++ S07_110_Reverse_Shift_an_Array.cpp
@@ -0,0 +1,19 @@
+/*
+
+Reverse - Reverse the element order in the array
+
+Left shift - Shifts all the elements one position to the
+             left of the array
+Ex : [1, 2, 3] becomes [2, 3, 0]
+
+Left rotation - Shifts all the elements one position to the
+                left of the array and the first element 
+                to the last of the array
+Ex : [1, 2, 3] becomes [2, 3, 1]
+
+Right shift(or Right rotation ) - Shifts all the elements on position to the
+                                  right of the array and the last element 
+                                  to the first of the array
+Ex : [1, 2, 3] becomes [3, 1, 2]
+
+*/
