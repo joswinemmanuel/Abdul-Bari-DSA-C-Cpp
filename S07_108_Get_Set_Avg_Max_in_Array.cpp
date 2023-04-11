442d58b90a8e63c74c25ebd521af37bf36d753e8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-04-11 22:25:56 +0530

    108

---
+++ S07_108_Get_Set_Avg_Max_in_Array.cpp
@@ -0,0 +1,18 @@
+/*
+
+Get() - Get(index)
+        To get the array element in the index
+
+Set() - Set(index, val)
+        To set the array index element value to val
+
+Sum() - Sum()
+        Returns the sum of elements in the array
+
+Avg() - Avg()
+        Returns the average value of elements in the array
+
+Max() - Max()
+        Returns the maximum value in the array
+
+*/
