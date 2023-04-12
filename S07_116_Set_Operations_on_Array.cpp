c8b9a18f1a45ca0636ee44c20c896576b2815bcb
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-04-12 20:23:00 +0530

    116

---
+++ S07_116_Set_Operations_on_Array.cpp
@@ -0,0 +1,12 @@
+/*
+
+Union   - union of two array will have all the elements in both
+          array without any duplicate values
+
+Intersection - It is an array having the common elements of 
+               both the arrays
+
+Difference - Difference of array A with array B
+             is all the elements in A that are not in B
+
+*/
