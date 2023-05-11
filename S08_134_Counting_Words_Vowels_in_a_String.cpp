90d533434902981a07daf8d034bc51e704ddb960
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-05-11 15:24:14 +0530

    134

---
+++ S08_134_Counting_Words_Vowels_in_a_String.cpp
@@ -0,0 +1,26 @@
+/*
+
+// How to count number of Vowels and Consonants
+int main() {
+    char A[] = "How are you";
+    int vowels=0, consonants=0;
+    for(int i=0; A[i]!='\0'; i++) {
+        if(A[i]=='A' || A[i]=='E' || A[i]=='I' || A[i]=='O' || A[i]=='U' || A[i]=='a' || A[i]=='e' || A[i]=='i' || A[i]=='o' || A[i]=='u')
+            vowels++;
+        else if(A[i]>=65 && A[i]<=90 || A[i]>=97 && A[i]<=122)
+            consonants++;
+    }
+}
+
+// How to count number of words
+int main() {
+    char A[] = "How are you";
+    int words=;
+    for(int i=0; A[i]!='\0'; i++) {
+        if(A[i]==' ' && A[i-1]!=' ')
+            words++;
+    }
+    printf("%d number of words", ++word);
+}
+
+*/
